8e569aff729fff1849033437cf8c7b9c7e5021c4
Karthik Hosur <rheuzaki@gmail.com>
2022-12-02 14:36:02 -0800

    Add files via upload

--- backend/memes/models.py
+++ backend/memes/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-    
+
+class Subscribers(models.Model):
+    email_id = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Subscribers"
+        db_table = 'subscribers'
